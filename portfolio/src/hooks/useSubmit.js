2026f1d6df69617239929b726a7de4080aba48e7
Jason Kobuchi <jekobuchi@gmail.com>
2023-02-14 10:02:15 -0800

    changed random to trigger 100% of the time to test

--- portfolio/src/hooks/useSubmit.js
+++ portfolio/src/hooks/useSubmit.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -14,25 +14,25 @@ const useSubmit = () => {
     const random = Math.random();
     setLoading(true);
     try {
-      await wait(2000);
+      await wait(1000);
       if (random < 0.5) {
         throw new Error("Something went wrong");
       }
       setResponse({
-        type: 'success',
+        type: "success",
         message: `Thanks for your submission ${data.firstName}, we will get back to you shortly!`,
-      })
+      });
     } catch (error) {
       setResponse({
-        type: 'error',
-        message: 'Something went wrong, please try again later!',
-      })
+        type: "error",
+        message: "Something went wrong, please try again later!",
+      });
     } finally {
       setLoading(false);
     }
   };
 
   return { isLoading, response, submit };
-}
+};
 
 export default useSubmit;
